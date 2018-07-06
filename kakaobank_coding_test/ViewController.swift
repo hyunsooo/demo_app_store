55a76fcd26a77ffca7a41df58ff622225a770fe7
hyunsoo han <dreamh1123@naver.com>
2018-07-06 22:43:26 +0900

    Initial Commit

---
+++ kakaobank_coding_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kakaobank_coding_test
+//
+//  Created by hyunsu han on 2018. 7. 6..
+//  Copyright © 2018년 hyunsoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
